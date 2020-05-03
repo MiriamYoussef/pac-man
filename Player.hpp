f9874571015b690a972e8310244f8e16a9343296
MiriamYoussef <64065274+MiriamYoussef@users.noreply.github.com>
2020-05-03 18:52:26 +0200

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -7,20 +7,21 @@
 #include<fstream>
 using namespace std;
 using namespace sf;
-
 class Player
 {
 private:
-
     int row, column;
-    Texture right, left, up,down;
-    RectangleShape shape;
+    Texture right, left, up, down;
 
 public:
-    Player(int initialrow, int initialcol,string rightimage, string leftimage, string upimage, string downimage);
-    void DrawOnWindow (RenderWindow&);
-    void Move(char direction, int [][28]);
-    int GetRow ();
-    int GetColumn ();
+    Player();
+    Player(int initialrow, int initialcol, string rightimage, string leftimage, string upimage, string downimage);
+    void DrawOnWindow(RenderWindow&);
+    void Move(char direction, int[34][28]);
+    int GetRow();
+    int GetColumn();
+    RectangleShape pac;
+
 };
+ 
 #endif /* Player_hpp */
