00337f59502621660eeafc1c28f53707c2a75ad9
MiriamYoussef <64065274+MiriamYoussef@users.noreply.github.com>
2020-05-03 19:34:15 +0200

    Update Game.hpp

--- Game.hpp
+++ Game.hpp
@@ -1,37 +1,40 @@
 
 #ifndef Game_hpp
 #define Game_hpp
-#include "Board.hpp"
-#include "Player.hpp"
-#include "Ghosts.hpp"
+#include "Board.h"
+#include "Player.h"
+#include "Ghosts.h"
 #include "sstream"
 
 class Game
 {
 private:
-    const int rows=34;
-    const int cols=28;
+    const int rows = 34;
+    const int cols = 28;
     int arr[34][28];
     int pacmanlives;
-    RectangleShape life1,life2,life3;
+    RectangleShape life1, life2, life3;
     Texture transparent, life;
     RenderWindow window;
     Text scoretext, highscoretext, livestext, starttext, endtext, scorenumbers, highscorenumbers;
     Font font;
     int score;
     int highscore;
+    Player pacman;
+    Board board;
+    Ghost ghost;
     void LivesConstructor();
     void Display_lives();
     void Display_score();
-    bool pellet(int, int);
-    bool powerpellet(int, int);
-    bool checkifEaten(int, int, int, int);
+    void pelleteaten(int arr[24][38]);
+    void pacmaneaten(int arr[24][38]);
     void computeLives();
-    void frightmode();
- 
+    bool frightmode();
+
 public:
     Game();
     void Start();
     void End();
+    
 };
 #endif 
