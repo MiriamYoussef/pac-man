f1e26789ccf55f4a638dfe3e76435728129dd2cc
MiriamYoussef <64065274+MiriamYoussef@users.noreply.github.com>
2020-05-03 19:37:05 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,81 +1,94 @@
 
 
-#include "Player.hpp"
+#include "Player.h"
+#include<SFML/Graphics.hpp>
+#include<SFML/Audio.hpp>
+#include<iostream>
+#include "Board.h"
+#include<fstream> 
+using namespace std;
+using namespace sf;
+
+Player::Player()
+{
+
+}
+
 Player::Player(int initialrow, int initialcol, string rightimage, string leftimage, string upimage, string downimage)
 {
     row = initialrow;
     column = initialcol;
-    shape.setSize(Vector2f(40, 40));
-    shape.setPosition(40 + 40 *initialcol , 40 + 40*initialrow );
+    pac.setSize(Vector2f(40, 40));
+    pac.setPosition(40 + 40 * initialcol, 40 + 40 * initialrow);
     right.loadFromFile(rightimage);
     left.loadFromFile(leftimage);
     up.loadFromFile(upimage);
     down.loadFromFile(downimage);
-    shape.setTexture(&left);
+    pac.setTexture(&left);
 }
-void Player::DrawOnWindow (RenderWindow& window)
+
+void Player::DrawOnWindow(RenderWindow& window)
 {
-    window.draw(shape);
+    window.draw(pac);
 }
 void Player::Move(char direction, int arr[34][28])
 {
-    if(tolower(direction)=='u')
+    if (tolower(direction) == 'u')
     {
-        shape.setTexture(&up);
+        pac.setTexture(&up);
         if (arr[row - 1][column] != 1)
         {
-            shape.move(0, -40);
+            pac.move(0, -40);
             row--;
         }
     }
-    else if(tolower(direction)=='d')
+    else if (tolower(direction) == 'd')
     {
-        shape.setTexture(&down);
+        pac.setTexture(&down);
         if (arr[row + 1][column] != 1)
-                {
-                    shape.move(0, 40);
-                    row++;
-                }
-       
-    }
-    else if(tolower(direction)=='l')
-    {
-        shape.setTexture(&left);
-       if (arr[row][column - 1] != 1)
-                {
-                    shape.move(-40, 0);
-                    column--;
-                }
-        if (arr[row][column]==4)
-               {
-                   row=16;
-                   column=27;
-                   shape.setPosition(40+40*column, 40+40*row);
-               }
-    }
-    else if(tolower(direction)=='r')
-    {
-         shape.setTexture(&right);
-        if (arr[row][column + 1] != 1)
-                {
-                    shape.move(40, 0);
-                    column++;
-                }
-        if (arr[row][column]==4)
         {
-            row=16;
-            column=0;
-            shape.setPosition(40+40*column, 40+40*row);
+            pac.move(0, 40);
+            row++;
+        }
+
+    }
+    else if (tolower(direction) == 'l')
+    {
+        pac.setTexture(&left);
+        if (arr[row][column - 1] != 1)
+        {
+            pac.move(-40, 0);
+            column--;
+        }
+        if (arr[row][column] == 4)
+        {
+            row = 16;
+            column = 27;
+            pac.setPosition(40 + 40 * column, 40 + 40 * row);
+        }
+    }
+    else if (tolower(direction) == 'r')
+    {
+        pac.setTexture(&right);
+        if (arr[row][column + 1] != 1)
+        {
+            pac.move(40, 0);
+            column++;
+        }
+        if (arr[row][column] == 4)
+        {
+            row = 16;
+            column = 0;
+            pac.setPosition(40 + 40 * column, 40 + 40 * row);
         }
     }
 }
 
-int Player::GetRow ()
+int Player::GetRow()
 {
     return row;
 }
-int Player::GetColumn ()
+int Player::GetColumn()
 {
     return column;
 }
-
